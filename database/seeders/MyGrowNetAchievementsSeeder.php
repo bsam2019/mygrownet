b9abc5e007e6d601fc6217c584587346021f5048
Bsam2019 <samsonbnd21@gmail.com>
2025-10-20 11:29:41 +0200

    Fix MyGrowNetAchievementsSeeder to handle database schema mismatches gracefully

--- database/seeders/MyGrowNetAchievementsSeeder.php
+++ database/seeders/MyGrowNetAchievementsSeeder.php
@@ -17,21 +17,28 @@ class MyGrowNetAchievementsSeeder extends Seeder
     private function seedAchievements(): void
     {
         $achievements = Achievement::createMyGrowNetAchievements();
+        $created = 0;
+        $skipped = 0;
         
         foreach ($achievements as $achievementData) {
-            Achievement::updateOrCreate(
-                ['slug' => $achievementData['slug']],
-                $achievementData
-            );
+            try {
+                Achievement::updateOrCreate(
+                    ['slug' => $achievementData['slug']],
+                    $achievementData
+                );
+                $created++;
+            } catch (\Exception $e) {
+                $this->command->warn('Skipped achievement: ' . $achievementData['slug'] . ' - ' . $e->getMessage());
+                $skipped++;
+            }
         }
 
-        $this->command->info('Created ' . count($achievements) . ' MyGrowNet achievements');
+        $this->command->info("Created {$created} MyGrowNet achievements" . ($skipped > 0 ? " (skipped {$skipped})" : ''));
     }
 
     private function seedLeaderboards(): void
     {
         $leaderboards = Leaderboard::createDefaultLeaderboards();
-        
         $this->command->info('Created ' . count($leaderboards) . ' default leaderboards');
     }
 }
