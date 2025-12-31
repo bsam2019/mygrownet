0be40cdbfa0b96343677badf4fed70eb94a83f4d
Bsam2019 <samsonbnd21@gmail.com>
2025-12-31 19:10:52 +0200

    fix: add group_name to site_permissions insert

--- database/migrations/2025_12_29_100000_add_products_permissions_to_site_permissions.php
+++ database/migrations/2025_12_29_100000_add_products_permissions_to_site_permissions.php
@@ -25,6 +25,7 @@ return new class extends Migration
                     'slug' => $permission['slug'],
                     'name' => $permission['name'],
                     'description' => $permission['description'],
+                    'group_name' => 'Products',
                 ]);
             }
         }
