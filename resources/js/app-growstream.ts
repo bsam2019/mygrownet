6a2f9ccf3de4410d9857174fc8da60661019b0f7
Bsam2019 <samsonbnd21@gmail.com>
2026-08-06 14:41:47 +0200

    Disable GrowStream service worker - fixing video playback fetch errors

--- resources/js/app-growstream.ts
+++ resources/js/app-growstream.ts
@@ -4,7 +4,18 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import type { DefineComponent } from 'vue';
 import { bootInertia, registerModuleSW } from './modules/createApp';
 
-registerModuleSW('/sw.js', 'GrowStream');
+// Unregister existing service workers that might be causing issues
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.getRegistrations().then((registrations) => {
+        registrations.forEach((registration) => {
+            console.log('[GrowStream] Unregistering service worker:', registration.scope);
+            registration.unregister();
+        });
+    });
+}
+
+// Temporarily disabled - service worker causing video playback issues
+// registerModuleSW('/sw.js', 'GrowStream');
 
 bootInertia('GrowStream', (name: string) => {
     return resolvePageComponent(
